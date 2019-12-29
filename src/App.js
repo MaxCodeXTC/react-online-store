244efd181a755e33a6332a5b358531f4af797f67
Hamza Shahid <hammzzeeee@gmail.com>
2019-12-29 14:04:53 +0000

    new changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom"
 
 import HomePage from "./pages/HomePage/HomePage"
 import ShopPage from "./pages/ShopPage/ShopPage"
+import SignInSignUp from "./pages/SignInSignUp/SignInSignUp"
 import Header from "./components/Header/Header"
 
 //overview
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUp} />
       </Switch>
     </div>
   );
